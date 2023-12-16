54943e8c78ad161bf871110a2da1c16180fcef09
Nauris Pūķis <hi@pyronaur.com>
2023-12-16 13:08:03 +0200

    Reduced Test Case

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	const test = $state({ value: 42 });
+	$effect(() => {
+		console.log("matches", test);
+	});
+
+	setTimeout(() => {
+		test.value = 55;
+	}, 1000);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
